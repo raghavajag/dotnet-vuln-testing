8592cebb44c68a8be817c533d9c95f478461d990
raghavajag <raghavajagarwal@gmail.com>
2025-05-19 06:25:02 +0000

    add

--- Program.cs
+++ Program.cs
@@ -12,6 +12,16 @@ var app = builder.Build();
 // Map controllers
 app.MapControllers();
 
+// Add a default route to call the vulnerable endpoint for demonstration
+app.MapGet("/vuln-demo", (Microsoft.AspNetCore.Mvc.ControllerBase controller, Microsoft.AspNetCore.Http.HttpRequest request) =>
+{
+    // Simulate a vulnerable call to the UserController's GetUser method
+    // This is for demonstration/testing tools like Semgrep
+    var username = request.Query["username"].ToString();
+    var userController = new VulnerableWebApp.Controllers.UserController();
+    return userController.VulnerableFunction(username);
+});
+
 app.Run();
 
 // See https://aka.ms/new-console-template for more information
